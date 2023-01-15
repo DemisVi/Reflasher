2417497038ebd38001506d59aa243974bba944cc
DemisVi <87558735+DemisVi@users.noreply.github.com>
2023-01-15 17:08:28 +0300

    Add test process. Write process awaiting

--- Writer.cs
+++ Writer.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Management;
+using System.Diagnostics;
 
 namespace Reflasher
 {
@@ -17,10 +18,22 @@ namespace Reflasher
         private readonly string? _cuName;
         private readonly static object _lock = new();
 
+        private readonly Process flasherProc;
+
         public Writer(StatusReporter reporter, string? cuName)
         {
             _reporter = reporter;
             _cuName = cuName;
+            flasherProc = new()
+            {
+                StartInfo = new()
+                {
+                    FileName = "./test.exe",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                },
+            };
         }
 
         public void Run() => Task.Factory.StartNew(Write);
@@ -34,21 +47,29 @@ namespace Reflasher
                 counter++;
                 lock (_lock)
                 {
-                    watcher.Query = WqlQueries.creationTsQuery;
-                    watcher.WaitForNextEvent();
-                    watcher.Stop();
-
-                    _reporter.Report(Thread.CurrentThread.ManagedThreadId.ToString() + " reporting " + _cuName);
+                    // watcher.Query = WqlQueries.creationTsQuery;
+                    // watcher.WaitForNextEvent();
+                    // watcher.Stop();
+                    flasherProc.Start();
+                    _reporter.Report(_cuName + " going report " + (flasherProc.StandardOutput.ReadLine().Contains("0") ? "SUCESS" : "FAIL"));
                     _reporter.Color(Brushes.Wheat);
 
+                    Thread.Sleep(new TimeSpan(0, 0, new Random().Next(10, 20)));
+
+                    _reporter.Color(Brushes.Aqua);
                     // some process launch work
 
-                    watcher.Query = WqlQueries.deletionTsQuery;
-                    watcher.WaitForNextEvent();
-                    watcher.Stop();
+                    // watcher.Query = WqlQueries.deletionTsQuery;
+                    // watcher.WaitForNextEvent();
+                    // watcher.Stop();
                 }
+                _reporter.Report(_cuName + " reporting " + (flasherProc.StandardOutput.ReadToEnd().Contains("0") ? "SUCESS" : "FAIL"));
+                flasherProc.WaitForExit();
+                if (flasherProc.ExitCode != 0) _reporter.Color(Brushes.LightPink);
+                else _reporter.Color(Brushes.LightGreen);
+                Thread.Sleep(2222);
                 _reporter.Color(Brushes.White);
-                if (counter % 2 == 0 && counter > 0) return;
+                // if (counter % 2 == 0 && counter > 0) return;
                 Thread.Sleep(111);
             }
         }
